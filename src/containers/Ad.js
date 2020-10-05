1401f252c84e224cc9920a20b94c1bf0e3839c20
marshall405 <marshall.slemp@gmail.com>
2020-10-05 16:44:01 -0500

    adds / to routes for place component

--- src/containers/Ad.js
+++ src/containers/Ad.js
@@ -5,7 +5,7 @@ export default function Ad({ ad }) {
     let path = useLocation().pathname
 
     return (
-        <Link to={`${path === '/' ? 'home' : path}/${ad.place_id}`}>
+        <Link to={`${path === '/' ? 'home/' : path}/${ad.place_id}`}>
             <div className="ad adShow">
                 <h1> {ad.name}</h1>
                 {/* <img className="location-icon" src={ad.icon} alt={ad.name} /> */}
