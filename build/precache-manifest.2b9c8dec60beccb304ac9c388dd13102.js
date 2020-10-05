4a31388b52ae56071c854c150daedb296d8961f1
marshall405 <marshall.slemp@gmail.com>
2020-10-05 14:10:08 -0500

    adds /build to github

---
+++ build/precache-manifest.2b9c8dec60beccb304ac9c388dd13102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea17847212d22cf874a215582def668",
+    "url": "/index.html"
+  },
+  {
+    "revision": "682035fc96e280a3c064",
+    "url": "/static/css/main.3f933cd2.chunk.css"
+  },
+  {
+    "revision": "549e32b0202473e05416",
+    "url": "/static/js/2.153a5baf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.153a5baf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682035fc96e280a3c064",
+    "url": "/static/js/main.1270170f.chunk.js"
+  },
+  {
+    "revision": "acea13b2280fbc2a01bc",
+    "url": "/static/js/runtime-main.387c2873.js"
+  },
+  {
+    "revision": "87362666fbd58104d572eaa613e71fd0",
+    "url": "/static/media/logo.87362666.png"
+  }
+]);
