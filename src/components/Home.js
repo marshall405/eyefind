ead14033d704c389ed18ffd93020f95c80dfad58
marshall405 <marshall.slemp@gmail.com>
2020-10-16 13:40:50 -0500

    removes unused code in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import SponsoredAds from './SponsoredAds'
 import Ads from './Ads'
 
-import fetchData from '../scripts/fetchData'
-
 export default function Home(props) {
-
-    const { places } = props
-
-    useEffect(() => {
-        let isCancelled = false;
-        const abortController = new AbortController();
-
-        fetchData(['any'], abortController)
-            .then(data => {
-                if (!isCancelled) {
-                    props.setPlaces(data)
-                }
-            })
-
-        return () => {
-            isCancelled = true;
-
-        }
-    }, [])
-
+    let places = []
     return (
         <div className="main home">
             <SponsoredAds ads={places} />
