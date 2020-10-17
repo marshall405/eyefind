08141510c477209e41f66c6b20b58d174f7ede29
marshall405 <marshall.slemp@gmail.com>
2020-10-17 12:16:53 -0500

    refactors ad.js - no functional changes

--- src/containers/Ad.js
+++ src/containers/Ad.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 export default function Ad({ video }) {
-    let path = useLocation().pathname
     return (
-
         <div className="ad adShow">
-            <iframe title="Youtube Video - GTA" id="player" type="text/html" width="100%" height="100%" allowFullScreen
-                src={`https://www.youtube.com/embed/${video}`}
+            <iframe
+                title="Youtube Video - GTA"
+                id={`player${video}`}
+                type="text/html"
+                width="100%" height="100%"
                 frameBorder=""
-                onClick={e => console.log(e.data)}
+                allowFullScreen
+                src={`https://www.youtube.com/embed/${video}`}
+                onMouseEnter={autoPlay}
             >
             </iframe>
         </div>
